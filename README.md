39bfaa5ce2a33c042f58ac2ce5d6656d3380a5b9
Oz Elentok <oz.elen@gmail.com>
2017-05-02 11:12:33 +0300

    Create deoplete source for gtags

---
+++ README.md
@@ -0,0 +1,13 @@
+# deoplete-gtags #
+deoplete-gtags is a deoplete.nvim source for GNU Global
+
+## Requirments ##
+- GNU Global
+- deoplete.nvim
+
+## Installation ##
+
+For dein.vim
+```vim
+call dein#add('ozelentok/deoplete-gtags')
+```
---
+++ rplugin/python3/deoplete/source/gtags_completion.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+sys.path.insert(1, os.path.dirname(__file__))
+from deoplete_gtags import GtagsBase # pylint: disable=locally-disabled, wrong-import-position
+
+class Source(GtagsBase):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'gtags'
+        self.mark = '[gtags]'
+
+    @classmethod
+    def get_search_flags(cls):
+        return ['-c']
+
+    @classmethod
+    def get_search_word(cls, context):
+        return context['input']
+
+    def gather_candidates(self, context):
+        word = self.get_search_word(context)
+        tags = self.exec_global(self.get_search_flags() + [word], context)
+        candidates = self._convert_to_candidates(tags)
+        return candidates
+
+    @classmethod
+    def _convert_to_candidates(cls, tags):
+        return [{'word': t} for t in tags]
