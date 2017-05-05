25243a90c616ca83652d812d0909b696e6c1f8e5
Oz Elentok <oz.elen@gmail.com>
2017-05-05 17:08:55 +0300

    Fix error on search with regex characters
    Regex characters cannot be used with global -c option

--- rplugin/python3/deoplete/sources/gtags_completion.py
+++ rplugin/python3/deoplete/sources/gtags_completion.py
@@ -6,6 +6,11 @@ from deoplete_gtags import GtagsBase # pylint: disable=locally-disabled, wrong-i
 
 class Source(GtagsBase):
 
+    FORRBIDDEN_CHARACTERS = [
+        '^', '$', '{', '}', '(', ')', '.',
+        '*', '+', '[', ']', '?', '\\'
+    ]
+
     def __init__(self, vim):
         super().__init__(vim)
 
@@ -20,8 +25,17 @@ class Source(GtagsBase):
     def get_search_word(cls, context):
         return '"{}"'.format(context['input'])
 
+    @classmethod
+    def is_word_valid_for_search(cls, word):
+        for forbbiden_char in cls.FORRBIDDEN_CHARACTERS:
+            if forbbiden_char in word:
+                return False
+        return True
+
     def gather_candidates(self, context):
         word = self.get_search_word(context)
+        if not self.is_word_valid_for_search(word):
+            return []
         tags = self.exec_global(self.get_search_flags() + [word], context)
         candidates = self._convert_to_candidates(tags)
         return candidates
